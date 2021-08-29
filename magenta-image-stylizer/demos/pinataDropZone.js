4d65c4786cb88a06baa43550afdcb7e63d53b871
Tristan <trungupdate@gmail.com>
2021-08-29 23:51:35 +0700

    drag files Pinata

---
+++ magenta-image-stylizer/demos/pinataDropZone.js
@@ -0,0 +1,41 @@
+Dropzone.autoDiscover = false;
+const pinataApiKey = "add your own keys here";
+const pinataSecretApiKey = "add your own keys here";
+// const pinataApiKey = "add your own keys here";
+// const pinataSecretApiKey = "add your own keys here";
+
+$("#dropzone").dropzone({
+  previewsContainer: ".dropzonePreview",
+  url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+  //maxFilesize: 2,
+  //maxFiles: 1,
+  acceptedFiles: ".jpeg,.jpg,.png",
+  //uploadMultiple: true,
+  //parallelUploads: 1,
+  headers: {
+    pinata_api_key: pinataApiKey,
+    pinata_secret_api_key: pinataSecretApiKey,
+  },
+  init: function () {
+    this.on("sending", function (file, xhr, formData) {
+      const metadata = JSON.stringify({
+        name: "testname",
+        keyvalues: {
+          exampleKey: "exampleValue",
+        },
+      });
+      formData.append("pinataMetadata", metadata);
+    });
+  },
+  error: function (file, message) {
+    $(file.previewElement)
+      .addClass("dz-error")
+      .find(".dz-error-message")
+      .text(message.error);
+    console.log("ERROR: ", message.error);
+  },
+  success: function (file, response) {
+    console.log("SUCCESS: ", response);
+    $("#response_from_upload").html("Response: " + response.IpfsHash);
+  },
+});
