55b92db696a4a6308b06a63577d5b6e2778bcc56
Tristan <trungupdate@gmail.com>
2021-08-29 14:45:54 +0700

    update Pinata Flow app

--- pinata-party-frontend/src/App.js
+++ pinata-party-frontend/src/App.js
@@ -1,18 +1,21 @@
-// import logo from "./logo.svg";
-import "./App.css";
-import AuthCluster from "./AuthCluster";
-// import TokenData from "./TokenData";
+// File: ./src/App.js
 
-function App() {
+import React from "react";
+import { AuthCluster } from "./auth-cluster";
+import { InitCluster } from "./init-cluster";
+import { ProfileCluster } from "./profile-cluster";
+import { useCurrentUser } from "./hooks/current-user";
+
+export default function App() {
+  const cu = useCurrentUser();
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <div className="Auth">
-        <AuthCluster />
-        {/* <TokenData /> */}
-      </div>
+    <div>
+      <AuthCluster />
+      <InitCluster address={cu.addr} />
+      <ProfileCluster address={cu.addr} />
+      <ProfileCluster address="0xba1132bc08f82fe2" />
+      <ProfileCluster address="0xf117a8efa34ffd58" />
     </div>
   );
 }
-
-export default App;
